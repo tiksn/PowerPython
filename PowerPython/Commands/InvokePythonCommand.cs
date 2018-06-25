1e47372caa4aaa702e637fe2f58e228ec73f8b6f
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-06-25 21:48:39 +0300

    python script runner

---
+++ PowerPython/Commands/InvokePythonCommand.cs
@@ -0,0 +1,41 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.PowerPython.Commands
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "Python")]
+    public class InvokePythonCommand : Command
+    {
+        [Parameter]
+        public Dictionary<string, string> Arguments { get; set; }
+
+        [Parameter]
+        public string ScriptFile { get; set; }
+
+        protected override Task ProcessRecordAsync(CancellationToken cancellationToken)
+        {
+            var engine = Python.CreateEngine();
+
+            var scope = engine.CreateScope();
+
+            scope.SetVariable("params", Arguments);
+
+            ScriptSource source = engine.CreateScriptSourceFromFile(ScriptFile);
+
+            object result = source.Execute(scope);
+
+            var variables = scope.GetVariableNames().Where(name => scope.GetVariable(name)).ToArray();
+
+            WriteObject(variables, true);
+
+            return Task.CompletedTask;
+        }
+    }
+}
