2aa058a071cc89eefb33612dcd355bd20dc496f2
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-07-22 15:22:02 +0300

    module file added

--- PowerPython/Commands/InvokePythonCommand.cs
+++ PowerPython/Commands/InvokePythonCommand.cs
@@ -1,10 +1,8 @@
 ﻿using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
